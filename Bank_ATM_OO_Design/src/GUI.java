6612242eecc70a31c5241370f5e85513fc318861
Xiankang Wu <jerrywxk@gmail.com>
2019-10-15 10:44:20 -0400

    Add First GUI test

--- Bank_ATM_OO_Design/src/GUI.java
+++ Bank_ATM_OO_Design/src/GUI.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class GUI {
+    public static void main(String args[]) {
+        JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        JButton button = new JButton("Press");
+        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.setVisible(true);
+    }
 }
