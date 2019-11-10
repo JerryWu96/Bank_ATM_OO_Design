125f784bc39d5bf48ca07214f44d538f56e0a981
PhoenixTAN <tanziqi1756@outlook.com>
2019-11-09 20:08:40 -0500

    Manager panel and Stock manipulating panel updated.

--- Bank_ATM_OO_Design/src/gui/ManagerPanel.java
+++ Bank_ATM_OO_Design/src/gui/ManagerPanel.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.ListIterator;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -17,17 +19,54 @@ Author: Ziqi Tan
 public class ManagerPanel extends JPanel implements ActionListener {
 	
 	private JTextArea report;
-	private JScrollPane jsp;
 	
 	public ManagerPanel() {
 		
-		JButton submit = new JButton("Get Report");
-		add(submit);
-		submit.addActionListener(this);
+		int windowHeight = OperationFrame.getInstance().getHeight();
+        int windowWidth = OperationFrame.getInstance().getWidth();
+        int x = windowWidth / 9 * 6;
+        int y = windowHeight / 10;
+        int increment = 50;
+        int buttonWidth = 150;
+        int buttonHeight = 25;
+        setLayout(null);
+		
+		JButton getDailyReportButton = new JButton("Get Daily Report");
+		getDailyReportButton.setBounds(x, y, buttonWidth, buttonHeight);
+		add(getDailyReportButton);
+		getDailyReportButton.addActionListener(this);
+		
+		JButton getReportButton = new JButton("Get Report");
+		getReportButton.setBounds(x, y+increment*1, buttonWidth, buttonHeight);
+		add(getReportButton);
+		getReportButton.addActionListener(this);
+		
+		JButton stockManipultingButton = new JButton("Stock Manipultor");
+		stockManipultingButton.setBounds(x, y+increment*2, buttonWidth, buttonHeight);
+		add(stockManipultingButton);
+		stockManipultingButton.addActionListener(this);
 		
 		JButton logoutButton = new JButton("Logout");
+		logoutButton.setBounds(x, y+increment*4, buttonWidth, buttonHeight);
 		add(logoutButton);
 		logoutButton.addActionListener(this);
+		
+		x = windowWidth/14;
+        y = windowHeight/20;
+        increment = 25;
+        int textWidth = 320;    
+        int textHeight = 330;
+        
+        JLabel accountInfoLabel = new JLabel("Report");
+        accountInfoLabel.setBounds(x, y,textWidth, 25);
+        add(accountInfoLabel);
+        
+        report = new JTextArea();
+        report.setLineWrap(true);
+        report.setEditable(false);
+        JScrollPane jsp = new JScrollPane(report);
+        jsp.setBounds(x, y+increment*1, textWidth, textHeight);
+        add(jsp);
 		
 	}
 	
@@ -42,6 +81,17 @@ public class ManagerPanel extends JPanel implements ActionListener {
 		}
 		
 		if( e.getActionCommand().equals("Get Report") ) {
+			
+		}
+		
+		if( e.getActionCommand().equals("Get Daily Report") ) {
+			
+		}
+		
+		if( e.getActionCommand().equals("Stock Manipultor") ) {
+			setEnabled(false);
+			setVisible(false);
+			OperationFrame.getInstance().setStockManipulatorPanel();
 			
 		}
 		
