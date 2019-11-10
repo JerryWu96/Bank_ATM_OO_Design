3221bf9a6fadb30be6b181dc5d143a1a697696c7
Xiankang Wu <jerrywxk@gmail.com>
2019-11-10 18:55:23 -0500

    Two more transactions: AccountOpen and AccountClose

---
+++ Bank_ATM_OO_Design/src/backend/AccountClose.java
@@ -0,0 +1,30 @@
+package backend;
+
+public class AccountClose extends Transaction {
+    private String accountType;
+    /**
+     * constructor for a checking/savings account
+     *
+     * @param userID
+     * @param creationDay
+     * @param accountType
+     */
+    AccountClose(String userID, int creationDay, String accountID, String accountType) {
+        super(userID, creationDay, accountID, SharedConstants.ACCOUNT_CLOSE);
+        this.accountType = accountType;
+    }
+
+    /**
+     * execute transaction: open a new account
+     *
+     * @return Error if the accountType is invalid
+     */
+    public String startTransaction() {
+        return BankPortal.getInstance().getBank().closeAccount(getUserID(), getAccountID(), accountType);
+    }
+
+    @Override
+    public String toString() {
+        return "Day " + getDay() + " customer " + getUserID() + " closed a " + this.accountType + " account";
+    }
+}
---
+++ Bank_ATM_OO_Design/src/backend/AccountOpen.java
@@ -0,0 +1,53 @@
+package backend;
+
+public class AccountOpen extends Transaction {
+    private String accountType;
+    private String savAccountID;
+
+    /**
+     * constructor for a checking/savings account
+     *
+     * @param userID
+     * @param creationDay
+     * @param accountType
+     */
+    AccountOpen(String userID, int creationDay, String accountType) {
+        super(userID, creationDay, SharedConstants.ACCOUNT_OPEN);
+        this.accountType = accountType;
+    }
+
+    /**
+     * constructor for a security account. Constructor chaining is applied.
+     *
+     * @param userID
+     * @param creationDay
+     * @param accountType
+     * @param savAccountID security account needs a savings account to work with
+     */
+    AccountOpen(String userID, int creationDay, String accountType, String savAccountID) {
+        this(userID, creationDay, accountType);
+        this.savAccountID = savAccountID;
+    }
+
+    /**
+     * execute transaction: open a new account
+     *
+     * @return Error if the accountType is invalid
+     */
+    public String startTransaction() {
+        switch (accountType) {
+            case SharedConstants.CK:
+                return BankPortal.getInstance().getBank().openAccount(getUserID(), accountType);
+            case SharedConstants.SAV:
+                return BankPortal.getInstance().getBank().openAccount(getUserID(), accountType);
+            case SharedConstants.SEC:
+                return BankPortal.getInstance().getBank().openAccount(getUserID(), accountType, savAccountID);
+        }
+        return SharedConstants.ERR_INVALID_TRANSACTION;
+    }
+
+    @Override
+    public String toString() {
+        return "Day " + getDay() + " customer " + getUserID() + " opened a " + this.accountType + " account";
+    }
+}
